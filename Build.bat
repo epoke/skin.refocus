3001fad7fb45ebded31e88b2b4740fa132bb3559
epoke <junghwan@gmail.com>
2014-04-13 15:48:53 +0900

    for korean

--- Build.bat
+++ Build.bat
@@ -6,33 +6,25 @@ md BUILD
 
 ECHO ------------------------------
 ECHO Building Skin Directory...
-xcopy "720p" "BUILD\skin.refocus\720p" /E /Q /I /Y
-xcopy "colors" "BUILD\skin.refocus\colors" /E /Q /I /Y
-xcopy "backgrounds" "BUILD\skin.refocus\backgrounds" /E /Q /I /Y
-xcopy "fonts" "BUILD\skin.refocus\fonts" /E /Q /I /Y
-xcopy "language" "BUILD\skin.refocus\language" /E /Q /I /Y
-xcopy "language\Danish" "BUILD\skin.refocus\language\Danish" /E /Q /I /Y
-xcopy "language\Dutch" "BUILD\skin.refocus\language\Dutch" /E /Q /I /Y
-xcopy "language\English" "BUILD\skin.refocus\language\English" /E /Q /I /Y
-xcopy "language\Finnish" "BUILD\skin.refocus\language\Finnish" /E /Q /I /Y
-xcopy "language\French" "BUILD\skin.refocus\language\French" /E /Q /I /Y
-xcopy "language\Italian" "BUILD\skin.refocus\language\Italian" /E /Q /I /Y
-xcopy "language\Korean" "BUILD\skin.refocus\language\Korean" /E /Q /I /Y
-xcopy "language\Polish" "BUILD\skin.refocus\language\Polish" /E /Q /I /Y
-xcopy "language\Portuguese (Brazil)" "BUILD\skin.refocus\language\Portuguese (Brazil)" /E /Q /I /Y
-xcopy "language\Romanian" "BUILD\skin.refocus\language\Romanian" /E /Q /I /Y
-xcopy "playlists" "BUILD\skin.refocus\playlists" /E /Q /I /Y
-xcopy "sounds" "BUILD\skin.refocus\sounds" /E /Q /I /Y
-xcopy "*.xml" "BUILD\skin.refocus\" /Q /I /Y
-xcopy "*.txt" "BUILD\skin.refocus\" /Q /I /Y
-xcopy "*.png" "BUILD\skin.refocus\" /Q /I /Y
-xcopy "*.jpg" "BUILD\skin.refocus\" /Q /I /Y
-xcopy "*.md" "BUILD\skin.refocus\" /Q /I /Y
+xcopy "720p" "BUILD\skin.refocus.korean\720p" /E /Q /I /Y
+xcopy "colors" "BUILD\skin.refocus.korean\colors" /E /Q /I /Y
+xcopy "backgrounds" "BUILD\skin.refocus.korean\backgrounds" /E /Q /I /Y
+xcopy "fonts" "BUILD\skin.refocus.korean\fonts" /E /Q /I /Y
+xcopy "language" "BUILD\skin.refocus.korean\language" /E /Q /I /Y
+xcopy "language\English" "BUILD\skin.refocus.korean\language\English" /E /Q /I /Y
+xcopy "language\Korean" "BUILD\skin.refocus.korean\language\Korean" /E /Q /I /Y
+xcopy "playlists" "BUILD\skin.refocus.korean\playlists" /E /Q /I /Y
+xcopy "sounds" "BUILD\skin.refocus.korean\sounds" /E /Q /I /Y
+xcopy "*.xml" "BUILD\skin.refocus.korean\" /Q /I /Y
+xcopy "*.txt" "BUILD\skin.refocus.korean\" /Q /I /Y
+xcopy "*.png" "BUILD\skin.refocus.korean\" /Q /I /Y
+xcopy "*.jpg" "BUILD\skin.refocus.korean\" /Q /I /Y
+xcopy "*.md" "BUILD\skin.refocus.korean\" /Q /I /Y
 
 ECHO ------------------------------
 ECHO Creating XBT File...
 START /B /WAIT TexturePacker.exe -input media\ -output media\Textures.xbt
-xcopy "media\*.xbt" "BUILD\skin.refocus\media\" /Q /I /Y
+xcopy "media\*.xbt" "BUILD\skin.refocus.korean\media\" /Q /I /Y
 
 REM ECHO ------------------------------
 REM ECHO Cleaning Up...
@@ -40,7 +32,7 @@ REM del "media\*.xbt"
 
 REM ECHO ------------------------------
 REM ECHO Copying Image Files...
-REM xcopy "media\*.*" "BUILD\skin.refocus\media\" /E /Q /I /Y
+REM xcopy "media\*.*" "BUILD\skin.refocus.korean\media\" /E /Q /I /Y
 
 echo Build Complete - Scroll Up to check for errors.
 pause
